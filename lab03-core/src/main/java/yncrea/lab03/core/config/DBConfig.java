c3bf950b5b815414699f373e79744f1d56fe75ba
Amaury Willemant <awillemant@gmail.com>
2019-09-16 11:20:25 +0200

    Update DBConfig.java

--- lab03-core/src/main/java/yncrea/lab03/core/config/DBConfig.java
+++ lab03-core/src/main/java/yncrea/lab03/core/config/DBConfig.java
@@ -5,7 +5,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
